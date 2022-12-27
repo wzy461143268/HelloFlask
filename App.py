e2ba83c157beff3b39ea2e92a36efb9838e183fa
Wang Zhongyao <wzy461143268@gmail.com>
2022-12-27 16:26:00 +0800

    Index

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import url_for, Flask
+from flask import url_for, Flask, render_template
 from markupsafe import escape
 
 # ...
@@ -26,3 +26,70 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+@app.route('/index')
+def index():
+    name = 'Grey Li'
+    movies = [
+        {
+            'title': 'My Neighbor Totoro',
+            'year': '1988'
+        },
+        {
+            'title': 'Dead Poets Society',
+            'year': '1989'
+        },
+        {
+            'title': 'A Perfect World',
+            'year': '1993'
+        },
+        {
+            'title': 'Leon',
+            'year': '1994'
+        },
+        {
+            'title': 'Mahjong',
+            'year': '1996'
+        },
+        {
+            'title': 'Swallowtail Butterfly',
+            'year': '1996'
+        },
+        {
+            'title': 'King of Comedy',
+            'year': '1999'
+        },
+        {
+            'title': 'Devils on the Doorstep',
+            'year': '1999'
+        },
+        {
+            'title': 'WALL-E',
+            'year': '2008'
+        },
+        {
+            'title': 'The Pork of Music',
+            'year': '2012'
+        },
+    ]
+    return render_template('index.html', name=name, movies=movies)
+
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
+
+
+@app.route('/contact')
+def contact():
+    return '<h1>Contact page</h1>'
+
+
+@app.route('/admin')
+def admin():
+    return '<h1>Welcome to the admin page</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
